e33b57f858262262d0ee74b1ee3a5fb6fcc2b4a2
Estifanos58 <122863605+Estifanos58@users.noreply.github.com>
2025-03-18 19:18:29 +0300

    fix: token for deployement

--- util/jwtToken.js
+++ util/jwtToken.js
@@ -6,11 +6,13 @@ export const generateJwtToken = (res, userId, role, email) => {
     const jwtToken = jwt.sign(payload, process.env.JWTTOKEN, { expiresIn: "7d" });
 
     res.cookie("token", jwtToken, {
-        sameSite: "lax", // Allows cookies across subdomains (use 'none' for cross-origin with credentials)
-        secure: process.env.NODE_ENV === "production", // Must be false for local development
+        httpOnly: true, // Prevents client-side JS from accessing the cookie
+        secure: process.env.NODE_ENV === "production", // true in production
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // Required for cross-origin cookies
         maxAge: 7 * 24 * 60 * 60 * 1000,
         path: "/",
     });
+    
     // console.log("Token Sent")
 
    //  res.json({ success: true, token: jwtToken }); // Also return the token as a response for debugging
